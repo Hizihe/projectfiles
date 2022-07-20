606b66ad763595f6630ac4c63c22ea9b38e8ba39
Adeleye Adelani <105894485+Hizihe@users.noreply.github.com>
2022-07-20 17:50:18 +0100

    Added my project

---
+++ index.js
@@ -0,0 +1,53 @@
+//document.write("Hello Word")
+/*
+this is a commet
+*/
+//this is a single line commet
+//let firstname;
+//let message;
+
+//firstname="iyanu"
+//message="Hello iyanu how are you doing?"
+
+
+//let surname = "Adeleye"
+//let count=9
+//let arr=['2']
+//let hope=true
+//document.write(type of name)
+/*
+let x = -1
+let a = 2
+let b = 3
+let total = a + b
+*/
+//console.log(total)
+
+//let score = 100
+
+
+
+//prompt()
+//confirm() 
+
+
+//+operation example on joining strings together
+//let firstname = "kenny"
+//let lastname = "adah"
+
+//let fullname = firstname+""+lastname
+
+//console.log(fullname)
+
+//let first = 1
+//let second = '2'
+
+//console.log("first", typeof first)
+//console.log("second", typeof second)
+
+
+//compairison in java
+let a = 10
+let b = 15
+let result = a > b
+console.log(result) 
